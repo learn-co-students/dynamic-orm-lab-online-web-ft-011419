bacb47dde129c0f21a7a3c2799334b0f3e723dc9
grangerl330 <grangerl330@gmail.com>
2019-02-13 13:32:17 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,13 +10,17 @@ class InteractiveRecord
 
   def self.column_names
     sql = "PRAGMA table_info('#{table_name}')"
+    # Retrieves the "hidden data from the specified table. This data will contain each column name"
 
     table_info = DB[:conn].execute(sql)
+    # Saves the "hidden data" to the variable table_info
+    binding.pry 
     column_names = []
 
     table_info.each {|column| column_names << column["name"]}
+    # Adds the "name" value of each column from the table_info variable and adds it to the column_names array
 
-    column_names.compact
+    column_names.compact # .compact removes any nil values
   end
 
   def initialize(options={})
@@ -30,6 +34,7 @@ class InteractiveRecord
   def col_names_for_insert
     self.class.column_names.delete_if {|col_name| col_name == "id"}.join(", ")
   end
+  # .delete_if removes all "id" columns from the array returned by column_names. We do not want to submit id columns when saving because the id column is created by the table automatically when a new row is added
 
   def values_for_insert
     values = []
